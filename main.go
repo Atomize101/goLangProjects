69c4d59d8898e05ce5db099aa22cb8274d206e7a
Chris Patrick <atomize101@gmail.com>
2021-06-11 16:26:19 -0400

    Strings and vars

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	nameOne := "Chris"           // short hand way of assigning a variable
+	var nameTwo string = "Sarah" // More traditional method
+	fmt.Println(nameOne, nameTwo)
+
+	var ageOne int = 20
+	ageTwo := 40 // Same method for ints
+	fmt.Println(ageOne, ageTwo)
+
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 99999999999999999999
+	scoreThree := 1.5
+
+	fmt.Println(scoreOne, scoreTwo, scoreThree)
+
 }
+
+// https://golang.org/ref/spec#Numeric_types
