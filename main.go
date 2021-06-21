06687c5b44afdc3a93b5dc7a99d29d72773a35a1
Chris Patrick <atomize101@gmail.com>
2021-06-21 11:03:57 -0400

    Using strings

--- main.go
+++ main.go
@@ -17,6 +17,28 @@ func main() {
 
 	fmt.Println(scoreOne, scoreTwo, scoreThree)
 
+	fmt.Print("Hello, ")
+	fmt.Print("world, \n")
+	fmt.Print("new line, \n")
+
+	age := 35
+	name := "Chris"
+
+	// Println %_ = format specifier
+	fmt.Println("hello")
+	fmt.Println("goodbye")                                //Println makes the new line automatically
+	fmt.Println("my age is", age, "and my name is", name) // output variables in printing
+
+	// Printf (formatted strings)
+	fmt.Printf("my age is %v and my name is %v \n", age, name)
+	fmt.Printf("my age is %q and my name is %q \n", age, name) // puts quotes around a string variable
+	fmt.Printf("age is of type %T \n", age)                    // Prints the type of the variable
+	fmt.Printf("You scored %0.1f points \n", 225.55)           //Prints a float
+
+	// Sprintf (save formatted strings)
+	var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
+	fmt.Println("the saved str is:", str)
+
 }
 
 // https://golang.org/ref/spec#Numeric_types
