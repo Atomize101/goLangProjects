e5b9eaeadc073178e185684e8318978ad185db2b
Chris Patrick <atomize101@gmail.com>
2021-06-24 13:04:20 -0400

    Package scope

--- main.go
+++ main.go
@@ -2,32 +2,18 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func getInitials(n string) (string, string) {
-	s := strings.ToUpper(n)
-	names := strings.Split(s, " ")
-
-	var initials []string
-	for _, v := range names {
-		initials = append(initials, v[:1])
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-
-	return initials[0], "_"
-}
+var score = 99.5 // If this was put in the main function it would not work
+// It has to be declared in the root
 
 func main() {
-	fn1, sn1 := getInitials("Chris Patrick")
-	fmt.Println(fn1, sn1)
 
-	fn2, sn2 := getInitials("Sarah Patrick")
-	fmt.Println(fn2, sn2)
+	sayHello("Chris")
 
-	fn3, sn3 := getInitials("Bob")
-	fmt.Println(fn3, sn3)
+	for _, v := range points {
+		fmt.Println(v)
+	}
+
+	showScore()
 }
