c98f4ab5b7c85a96eb61004c071399e7e070f94c
Chris Patrick <atomize101@gmail.com>
2021-06-29 13:01:15 -0400

    Saving the file

--- bill.go
+++ bill.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type bill struct {
 	name  string
@@ -50,4 +53,16 @@ func (b *bill) updateTip(tip float64) { // b dereferences the pointer so we can 
 // add an item to the bill
 func (b *bill) addItem(name string, price float64) {
 	b.items[name] = price
+}
+
+// save bill
+func (b *bill) save() {
+	data := []byte(b.format())
+
+	err := os.WriteFile("bills/"+b.name+".txt", data, 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Bill was saved to file")
+
 }
