bd125f56735bf7ea403230fd0be2a4ea3ba7b94f
Chris Patrick <atomize101@gmail.com>
2021-07-08 14:42:10 -0400

    Changing calc to use a switch/case

--- main.go
+++ main.go
@@ -10,17 +10,23 @@ func subtract(n1 int, n2 int) int {
 	return n1 - n2
 }
 
+var result int
+
 func main() {
-	//var operator string
-	total := 0
-	total2 := 0
+	var operator string
 	var number1, number2 int
+
 	fmt.Println("Please enter a number")
 	fmt.Scanln(&number1)
 	fmt.Println("Please enter the next number")
 	fmt.Scanln(&number2)
-	total = add(number1, number2)
-	total2 = subtract(number1, number2)
-	fmt.Printf("%d + %d = %d \n", number1, number2, total)
-	fmt.Printf("%d - %d = %d \n", number1, number2, total2)
+	fmt.Print("Please enter Operator (+,-,/,%,*):")
+	fmt.Scanln(&operator)
+	fmt.Printf("%d %s %d = %d \n", number1, operator, number2, result)
+
+	switch operator {
+	case "+":
+		result = add(number1, number2)
+		fmt.Println(result)
+	}
 }
